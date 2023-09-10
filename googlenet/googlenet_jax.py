fe811d137b018f5e733bd3cd61fc16701afc7ba6
wty-yy <wty-yy@github.com>
2023-09-10 21:27:53 +0800

    update VGG and googlenet

--- googlenet/googlenet_jax.py
+++ googlenet/googlenet_jax.py
@@ -4,7 +4,7 @@ sys.path.append(str(Path.cwd()))
 
 import jax, jax.numpy as jnp
 import flax, flax.linen as nn
-from flax.training.train_state import TrainState
+from flax.training import train_state
 import optax
 from tensorboardX import SummaryWriter
 from argparse import ArgumentParser
@@ -33,7 +33,7 @@ def get_args_and_writer():
     cvt2Path = lambda x: Path(x)
     str2bool = lambda x: x in ['yes', 'y', 'True', '1']
     parser = ArgumentParser()
-    parser.add_argument("--model-name", type=str, default="VGG16",
+    parser.add_argument("--model-name", type=str, default="GoogleNet",
         help="the name of the model")
     parser.add_argument("--wandb-track", type=str2bool, default=False, const=True, nargs='?',
         help="if taggled, track with wandb")
@@ -91,44 +91,91 @@ def get_args_and_writer():
     )
     return args, writer
 
-from typing import Sequence
-
-class ConvBlock(nn.Module):
-    cnn_size: int
-    block_size: int
-
-    @nn.compact
-    def __call__(self, x):
-        for _ in range(self.block_size):
-            x = nn.relu(nn.Conv(self.cnn_size, (3, 3))(x))
-        return nn.max_pool(x, (2, 2), (2, 2))
-
-class VGG(nn.Module):
-    cnn_sizes: Sequence[int]
-    block_sizes: Sequence[int]
+class Inception(nn.Module):
+    conv1x1: int
+    conv3x3: tuple[int]
+    conv5x5: tuple[int]
+    pool1x1: int
 
     @nn.compact
-    def __call__(self, x):
-        x = x / 255.0
-        for cnn_size, block_size in zip(self.cnn_sizes, self.block_sizes):
-            x = ConvBlock(cnn_size, block_size)(x)
+    def __call__(self, x):  # nn.Conv default padding is 'SAME'
+        path1 = nn.relu(nn.Conv(self.conv1x1, (1, 1))(x))
+        path2 = nn.relu(nn.Conv(self.conv3x3[0], (1, 1))(x))
+        path2 = nn.relu(nn.Conv(self.conv3x3[1], (3, 3))(path2))
+        path3 = nn.relu(nn.Conv(self.conv5x5[0], (1, 1))(x))
+        path3 = nn.relu(nn.Conv(self.conv5x5[1], (5, 5))(path3))
+        path4 = nn.max_pool(x, (3, 3), strides=(1, 1), padding='SAME')
+        path4 = nn.relu(nn.Conv(self.pool1x1, kernel_size=(1, 1))(path4))
+        return jnp.concatenate([path1, path2, path3, path4], axis=-1)
+
+class AuxilliaryOutput(nn.Module):
+    
+    @nn.compact
+    def __call__(self, x, train):
+        x = nn.avg_pool(x, (5, 5), strides=(3, 3))
+        x = nn.relu(nn.Conv(128, (1, 1))(x))
         x = x.reshape(x.shape[0], -1)
-        x = nn.relu(nn.Dense(4096)(x))
-        x = nn.relu(nn.Dense(4096)(x))
-        return nn.Dense(1000)(x)
+        x = nn.relu(nn.Dense(1024)(x))
+        x = nn.Dropout(0.7, deterministic=not train)(x)
+        x = nn.Dense(1000)(x)
+        return x
+
+class GoogleNet(nn.Module):
+    """Inception-v1"""
+
+    @nn.compact
+    def __call__(self, x, train: bool = True):
+        x = x / 255.0
+        x = nn.relu(nn.Conv(64, (7, 7), strides=2)(x))
+        x = nn.max_pool(x, (3, 3), strides=(2, 2), padding=((1, 1), (1, 1)))
+        x = nn.BatchNorm(use_running_average=not train)(x)
+        x = nn.relu(nn.Conv(64, (1, 1))(x))
+        x = nn.relu(nn.Conv(192, (3, 3))(x))
+        x = nn.BatchNorm(use_running_average=not train)(x)
+        x = nn.max_pool(x, (3, 3), strides=(2, 2), padding=((1, 1), (1, 1)))
+        x = Inception(conv1x1=64, conv3x3=(96, 128), conv5x5=(16, 32), pool1x1=32)(x)
+        x = Inception(conv1x1=128, conv3x3=(128, 192), conv5x5=(32, 96), pool1x1=64)(x)
+        x = nn.max_pool(x, (3, 3), strides=(2, 2), padding=((1, 1), (1, 1)))
+        x = Inception(conv1x1=192, conv3x3=(96, 208), conv5x5=(16, 48), pool1x1=64)(x)
+
+        out1 = AuxilliaryOutput()(x, train)
+
+        x = Inception(conv1x1=160, conv3x3=(112, 224), conv5x5=(24, 64), pool1x1=64)(x)
+        x = Inception(conv1x1=128, conv3x3=(128, 256), conv5x5=(24, 64), pool1x1=64)(x)
+        x = Inception(conv1x1=112, conv3x3=(144, 288), conv5x5=(32, 64), pool1x1=64)(x)
+
+        out2 = AuxilliaryOutput()(x, train)
+
+        x = Inception(conv1x1=256, conv3x3=(160, 320), conv5x5=(32, 128), pool1x1=128)(x)
+        x = nn.max_pool(x, (3, 3), strides=(2, 2), padding=((1, 1), (1, 1)))
+        x = Inception(conv1x1=256, conv3x3=(160, 320), conv5x5=(32, 128), pool1x1=128)(x)
+        x = Inception(conv1x1=384, conv3x3=(192, 384), conv5x5=(48, 128), pool1x1=128)(x)
+
+        x = nn.avg_pool(x, (7, 7), strides=(1, 1))
+        x = x.reshape(x.shape[0], -1)
+        x = nn.Dropout(0.4, deterministic=not train)(x)
+        out3 = nn.Dense(1000)(x)
+
+        return [out1, out2, out3]
+
 
 if __name__ == '__main__':
     args, writer = get_args_and_writer()
-    VGG16 = VGG(cnn_sizes=[64, 128, 256, 512, 512], block_sizes=[2, 2, 3, 3, 3])
-    VGG19 = VGG(cnn_sizes=[64, 128, 256, 512, 512], block_sizes=[2, 2, 4, 4, 4])
-    model = VGG16 if args.model_name == 'VGG16' else VGG19
+    model = GoogleNet()
     key = jax.random.PRNGKey(args.seed)
-    print(model.tabulate(key, jnp.empty(args.input_shape)))
+    print(model.tabulate(key, jnp.empty(args.input_shape), train=False))
 
+    class TrainState(train_state.TrainState):
+        batch_stats: dict
+        dropout_key: jax.random.KeyArray
+
+    variables = model.init(key, jnp.empty(args.input_shape), train=False)
     state = TrainState.create(
         apply_fn=model.apply,
-        params=model.init(key, jnp.empty(args.input_shape)),
-        tx=optax.adam(learning_rate=args.learning_rate)
+        params=variables['params'],
+        tx=optax.adam(learning_rate=args.learning_rate),
+        batch_stats=variables['batch_stats'],
+        dropout_key=jax.random.PRNGKey(args.seed),
     )
     
     save_id = args.load_weights_id + 1
@@ -153,16 +200,24 @@ if __name__ == '__main__':
     @partial(jax.jit, static_argnames='train')
     def model_step(state: TrainState, x, y, train: bool = True):
 
-        def loss_fn(params, x, y):
-            logits = state.apply_fn(params, x)
-            loss = -(y * jax.nn.log_softmax(logits)).sum(-1)
-            return loss.mean(), logits
+        def loss_fn(params):
+            logits_list, updates = state.apply_fn(
+                {'params': params, 'batch_stats': state.batch_stats},
+                x, train=train,
+                mutable=['batch_stats'],
+                rngs={'dropout': jax.random.fold_in(state.dropout_key, state.step)}
+            )
+            loss = 0
+            for logits, coef in zip(logits_list, (0.3, 0.3, 1.0)):
+                loss += -(y * jax.nn.log_softmax(logits)).sum(-1) * coef
+            return loss.mean(), (logits, updates)
         
         if train:
-            (loss, logits), grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params, x, y)
+            (loss, (logits, updates)), grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
             state = state.apply_gradients(grads=grads)
+            state = state.replace(batch_stats=updates['batch_stats'])
         else:
-            loss, logits = loss_fn(state.params, x, y)
+            loss, (logits, _) = loss_fn(state.params, x, y)
 
         y_cat = jnp.argmax(y, -1)
         top1 = (jnp.argmax(logits, -1) == y_cat).mean()
