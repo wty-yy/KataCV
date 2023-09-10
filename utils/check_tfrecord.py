12dddbfee9e6d04d03a1503a2fd695ebfb192e0b
wty-yy <wty-yy@github.com>
2023-09-10 21:53:45 +0800

    update check_tfrecord 对当前的tfrecord文件进行检查，并统计图片总数 修改默认的tfreord文件名称

--- utils/check_tfrecord.py
+++ utils/check_tfrecord.py
@@ -1,25 +1,28 @@
 # -*- coding: utf-8 -*-
 '''
-@File    : translate_tfrecord.py
-@Time    : 2023/09/08 21:32:50
+@File    : translate_tfrecord_check.py
+@Time    : 2023/09/10 21:29:04
 @Author  : wty-yy
 @Version : 1.0
 @Blog    : https://wty-yy.space/
 @Desc    : 
-该文件用于将dataset转化为TFRecord二进制形式，用于快速读入，需要先使用utils/make_label_json.py生成类别名称的对应编号json文件
-- 转化Imagenet2012数据集用时13:12:49，最终文件大小147GB
+对translate_tf_record.py保存好的tfrecord进行文件检查，并统计完好图片数目
 '''
+
+if __name__ == '__main__':
+    pass
+
 import tensorflow as tf, json, argparse
 from pathlib import Path
 from tqdm import tqdm
 import numpy as np
 
-path_origin_dataset = Path("/media/yy/Data/dataset/imagenet")
+path_origin_tfrecord = Path("/media/yy/Data/dataset/imagenet")
 path_logs = Path.cwd().joinpath("logs")
 def parse_args():
     cvt2path = lambda x: Path(x)
     parser = argparse.ArgumentParser()
-    parser.add_argument("--path-origin-dataset", type=cvt2path, default=path_origin_dataset,
+    parser.add_argument("--path-origin-tfrecord", type=cvt2path, default=path_origin_tfrecord,
         help="the path of the original dataset (JPEG)")
     parser.add_argument("--path-logs", type=cvt2path, default=path_logs,
         help="the path for saving the json logs")
@@ -30,24 +33,28 @@ def parse_args():
     parser.add_argument("--seed", type=int, default=1,
         help="the seed for shuffle the images' order")
     args = parser.parse_args()
-    args.path_subdataset = args.path_origin_dataset.joinpath(args.subfolder_name)
-    args.path_tfrecord = args.path_origin_dataset.joinpath(f"imagenet2012-{args.subfolder_name}-origin.tfrecord")
+    args.path_sub_origin_tfrecord = args.path_origin_tfrecord.joinpath(f"imagenet2012-{args.subfolder_name}-origin.tfrecord")
+    args.path_tfrecord = args.path_origin_tfrecord.joinpath(f"imagenet2012-{args.subfolder_name}.tfrecord")
     assert(args.path_logs.exists())
-    assert(args.path_origin_dataset.exists())
-    assert(args.path_subdataset.exists())
+    assert(args.path_origin_tfrecord.exists())
+    assert(args.path_sub_origin_tfrecord.exists())
     return args
 
-def translate_tfrecord(args):
-    image_paths = []
-    for subclass in sorted(args.path_subdataset.iterdir()):
-        if subclass.is_dir():
-            for image_path in subclass.iterdir():
-                if image_path.name[-4:].lower() == args.image_type:
-                    image_paths.append((str(image_path), nameid2label[subclass.name]))
-    print(f"find images for {args.subfolder_name}:", len(image_paths))
+def decode_jpeg_tfrecord(example):
+    feature = {
+        'image': tf.io.FixedLenFeature([], tf.string),
+        'label': tf.io.FixedLenFeature([], tf.int64),
+    }
+    example = tf.io.parse_single_example(example, feature)
+    image = tf.io.decode_jpeg(example['image'], channels=3)
+    return image, example['label']
 
-    def serialize_example(path, label):
-        bytes_image = tf.io.read_file(path)
+def check_tfrecord(args):
+    ds_tfrecord = tf.data.TFRecordDataset(args.path_sub_origin_tfrecord)
+    ds = ds_tfrecord.map(decode_jpeg_tfrecord).ignore_errors(log_warning=True)
+
+    def serialize_example(image, label):
+        bytes_image = tf.io.encode_jpeg(image)
         example = tf.train.Example(features=tf.train.Features(
             feature={
                 'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[bytes_image.numpy()])),
@@ -56,12 +63,12 @@ def translate_tfrecord(args):
         ))
         return example.SerializeToString()
 
-    image_paths = np.array(image_paths)
-    np.random.shuffle(image_paths)
-
     writer = tf.io.TFRecordWriter(str(args.path_tfrecord))
-    for path, label in tqdm(image_paths):
-        writer.write(serialize_example(path, int(label)))
+    count = 0
+    for image, label in tqdm(ds):
+        writer.write(serialize_example(image, int(label)))
+        count += 1
+    print(f"Available image in '{args.path_sub_origin_tfrecord}':", count)
     writer.close()
 
 if __name__ == '__main__':
@@ -69,4 +76,4 @@ if __name__ == '__main__':
     np.random.seed(args.seed)
     with open(args.path_logs.joinpath("nameid2label.json"), 'r') as file:
         nameid2label = json.load(file)
-    translate_tfrecord(args)
+    check_tfrecord(args)
