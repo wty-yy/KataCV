b4090f4a0fba4f3cd539c5601c7d9b6d4202a33c
wty-yy <wty-yy@github.com>
2023-09-13 11:58:14 +0800

    update utils plot cells

--- katacv/utils/detection/__init__.py
+++ katacv/utils/detection/__init__.py
@@ -4,10 +4,10 @@ import numpy as np
 
 def plot_box(ax: plt.Axes, image_shape: tuple[int], box_params: tuple[float] | np.ndarray, text=""):
     """
-    box_params: (x, y, w, h) is proportion of the image, so we need `image_shape`
+    params::box_params: (x, y, w, h) is proportion of the image, so we need `image_shape`
         - (x, y) is the center of the box.
         - (w, h) is the width and height of the box.
-    text: The text display in the upper left of the bounding box.
+    params::text: The text display in the upper left of the bounding box.
     """
     params, shape = box_params, image_shape
     x_min = int(shape[1]*(params[0]-params[2]/2))
@@ -22,6 +22,10 @@ def plot_box(ax: plt.Axes, image_shape: tuple[int], box_params: tuple[float] | n
         ax.text(x_min+2, y_min, text, color='white', backgroundcolor='red', va='bottom', ha='left', fontsize=8, bbox=bbox_props)
 
 def plot_cells(ax: plt.Axes, image_shape: tuple[int], S: int):
+    """
+    Draw the cells division on the image ax.
+    params::S: Split the image into SxS cells.
+    """
     x_size, y_size = int(image_shape[1] / S), int(image_shape[0] / S)
     for i in range(1,S):
         ax.plot([i*x_size, i*x_size], [0, image_shape[0]], c='b')
