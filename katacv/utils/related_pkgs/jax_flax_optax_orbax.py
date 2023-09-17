7d1b2240afe7e40421ed0d01d4810d0cb406c986
wty-yy <wty-yy@github.com>
2023-09-17 18:27:58 +0800

    update related_pkgs

--- katacv/utils/related_pkgs/jax_flax_optax_orbax.py
+++ katacv/utils/related_pkgs/jax_flax_optax_orbax.py
@@ -7,6 +7,8 @@ from katacv.utils.related_pkgs.jax import *  # jax, jnp, flax, nn, train_state, 
 import jax, jax.numpy as jnp
 import flax, flax.linen as nn
 from flax.training import train_state
+from flax.training import orbax_utils
 import optax
+import orbax.checkpoint as ocp
 
 from functools import partial
